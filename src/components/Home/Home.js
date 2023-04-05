82598a24246c89dab0d8013b2d8b856b338b8224
mjeder <matteder96@gmail.com>
2023-04-05 17:35:14 -0500

    Update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,36 +1,139 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Home2 from "./Home2";
 import Type from "./Type";
+import myImg from "../../Assets/profile-pic.jpg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function Home() {
   return (
-    <section>
-      <Container fluid className="home-section" id="home">
+      <Container fluid className="home-section-background" id="about">
         <Container className="home-content">
+            <Row>
+              <Col md={7} className="home-header">
+                <h1 className="heading">
+                 Hi there!{" "}
+                  <span className="wave" role="img" aria-labelledby="wave">
+                    👋🏻
+                  </span>
+                </h1>
+
+                <h1 className="heading">
+                  I'm
+                  <strong className="main-name"> Matt Eder</strong>
+                </h1>
+
+                <div className="heading-type">
+                  <Type />
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        <Container>
+          <div className="home-bio">
           <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi there!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
+            <Col md={8} className="home-about-description">
+              <h1 style={{ fontSize: "2.6em" }}>
+                LET ME <span className="light-blue"> INTRODUCE </span> MYSELF
               </h1>
-
-              <h1 className="heading-name">
-                I'm
-                <strong className="main-name"> Matt Eder</strong>
-              </h1>
-
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
+              <p className="home-about-body">
+                I fell in love with programming and I have at least learnt
+                something, I think… 🤷‍♂️
+                <br />
+                <br />I am fluent in classics like
+                <i>
+                  <b className="light-blue"> C++, Javascript and Python. </b>
+                </i>
+                <br />
+                <br />
+                My field of Interest's are building new &nbsp;
+                <i>
+                  <b className="light-blue">Web Technologies and Products </b> and
+                  also in areas related to{" "}
+                  <b className="light-blue">
+                    Deep Learning and Natural Launguage Processing.
+                  </b>
+                </i>
+                <br />
+                <br />
+                Whenever possible, I also apply my passion for developing products
+                with <b className="light-blue">Node.js</b> and
+                <i>
+                  <b className="light-blue">
+                    {" "}
+                    Modern Javascript Library and Frameworks
+                  </b>
+                </i>
+                &nbsp; like
+                <i>
+                  <b className="light-blue"> React.js and Next.js</b>
+                </i>
+              </p>
+            </Col>
+            <Col md={4} className="myAvtar">
+              <Tilt>
+                <img src={myImg} className="img-fluid" alt="profile" />
+              </Tilt>
+            </Col>
+          </Row>
+          </div>
+          <Row>
+            <Col md={12} className="home-about-social">
+              <h1>FIND ME ON</h1>
+              <p>
+                Feel free to <span className="light-blue">connect </span>with me
+              </p>
+              <ul className="home-about-social-links">
+                <li className="social-icons">
+                  <a
+                    href="https://github.com/soumyajit4419"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <AiFillGithub />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://twitter.com/Soumyajit4419"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <AiOutlineTwitter />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://www.linkedin.com/in/soumyajit4419/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://www.instagram.com/soumyajit4419"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour home-social-icons"
+                  >
+                    <AiFillInstagram />
+                  </a>
+                </li>
+              </ul>
             </Col>
           </Row>
         </Container>
       </Container>
-      <Home2 />
-    </section>
   );
 }
 
