a7faef61b6406849dd2037c11b388dc1a153b150
mjeder <matteder96@gmail.com>
2023-04-06 18:11:26 -0500

    Updates

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,41 +2,31 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
 
 function About() {
   return (
     <Container fluid className="about-section">
       <Container>
-        <Row style={{ justifyContent: "center", padding: "10px", alignItems: "center", textAlign: "center" }}>
-          <Col
-            md={7}
-            style={{
-              justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
-            }}
-          >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px", textAlign: "center" }}>
-              ABOUT<strong className="light-blue"> ME</strong>
-            </h1>
-            <Aboutcard />
-          </Col>
-          <Col
-            md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
-            className="about-img"
-          >
-            <img src={laptopImg} alt="about" className="img-fluid" />
-          </Col>
-        </Row>
+        {/* Bio Section */}
+        <Container className="about-me-container">
+          <Row style={{ justifyContent: "center" }}>
+            <Col>
+              <h1 style={{ fontSize: "2.1em", paddingBottom: "20px", textAlign: "center" }}>
+                ABOUT<strong className="light-blue"> ME</strong>
+              </h1>
+              <Aboutcard />
+            </Col>
+          </Row>
+        </Container>
+
+        {/* Skills Section */}
         <h1 className="project-heading">
           Professional <strong className="light-blue">Skillset </strong>
         </h1>
-
         <Techstack />
 
+        {/* Tools Section */}
         <h1 className="project-heading">
           <strong className="light-blue">Tools</strong> I use
         </h1>
--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,40 +1,41 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import { ImPointRight } from "react-icons/im";
+import { Container } from "react-bootstrap";
+
 
 function AboutCard() {
   return (
-    <Card className="quote-card-view">
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "center" }}>
-            Hi Everyone, I am <span className="light-blue">Soumyajit Behera </span>
-            from <span className="light-blue"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+    <Container className="about-bio">
+        <p>
+            I'm based in <b className="light-blue"> Chicago, IL</b> and currently looking for a new company to continue my career with. I'm open to remote or hybrid roles as a <b className="light-blue"> Front-End Engineer</b>, <b className="light-blue"> QA Engineer</b>, or <b className="light-blue"> Sales Engineer</b>.
+            
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
+            
+            Previously, I was a Front-End Engineer at <b className="light-blue"> DoorDash</b> on the Digital Ordering team, we focused on building DoorDash's first ever Order & Pay at the Table product. We successfully launched two major features - <b className="light-blue"> Group Ordering</b>, which allowed users to build an order collaboratively using their own devices & <b className="light-blue"> Order Now, Pay Later</b>, which gave users the opportunity to start tabs on their own devices using Digital Wallets instead of paying after each order. 
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
-        </blockquote>
-      </Card.Body>
-    </Card>
+            <br />
+            <br />
+
+            Prior to DoorDash, I was a <b className="light-blue"> Quality Assurance Engineer</b> at <b className="light-blue"> Bbot</b>, a restaurant technology startup (eventually acquired by DoorDash). This was my first experience in Software Development industry and allowed me the opportunity to grow into the developer I am today.
+
+            <br />
+            <br />
+
+            Before that I wore many different hats across the hospitality industry...
+            <br />
+            - Account Executive at Toast
+            <br />
+            - Jr. Product Manager, Topgolf Live at Topgolf 
+            <br />
+            - Venue Operations Manager at Topgolf
+            <br />
+            - Training Consultant at Topgolf
+            <br />
+            <br />
+            My background has provided me valuable customer facing experience and perspective that I channel when solving the fun and complex problems involved with software development.
+        </p>
+  </Container>
+
   );
 }
 
